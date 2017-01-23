6e72d715b81333f785c4bfde26603e2fe99d66af
Agboola Olasunkanmi <Agboola Olasunkanmi>
2017-01-23 23:47:39 +0300

    i decleared the getTimestampJson Function to handle the valid cases, and return null values for the invalid ones

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ app.get('/:timestamp', function(request, response) {
 app.listen(PORT, function() {
   console.log('Server is listening on port ' + PORT);
 });
+function getTimestampJSON(timestamp) {
+	var result = {
+		unix: null,
+		natural: null
+	};
+ 
+	var date;
+	if (!isNaN(parseInt(timestamp))) {
+		date = new Date(parseInt(timestamp));
+	} else {
+		date = new Date(timestamp);
+	}
+ 
+	if (!isNaN(date.getTime())) {
+		result.unix = date.getTime();
+		result.natural = getNaturalDate(date);
+	}
+    
+	return result;
+}
